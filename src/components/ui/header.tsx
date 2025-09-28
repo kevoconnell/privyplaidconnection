955fc8c7c9449e7826c5912e531a9daa1f2555a6
Kevin O'Connell <kevinoconnell42@gmail.com>
2025-09-27 19:51:26 -0700

    Update header.tsx

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import { SignInButton } from "../sign-in-button";
 const Header = () => {
   const router = useRouter();
   return (
-    <header className="mx-auto flex w-full max-w-4xl md:max-w-full items-center justify-between  py-4 sm:px-4 sm:py-5 md:py-6">
+    <header className="mx-auto flex w-full max-w-4xl md:max-w-full items-center justify-between space-x-3  py-4 sm:px-4 sm:py-5 md:py-6">
       <div className="flex items-center gap-2">
         <button
           onClick={() => router.push("/")}
